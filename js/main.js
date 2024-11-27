a881bf5a6a3a5b4185d6ba246e6540825bec1616
ultrasue <nastya.murlla@gmail.com>
2024-11-27 12:24:07 +0300

    add phone mask function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function restrictedInput() {
   const restrictedInputs = document.querySelectorAll(".restricted-input");
 
   restrictedInputs.forEach((input) => {
-    input.addEventListener("keypress", function (event) {
+    input.addEventListener("keydown", function (event) {
       if (event.key === "+" || event.key === "-") {
         event.preventDefault();
       }
@@ -50,9 +50,41 @@ function hideLayout() {
   }
 }
 
+function formatPhoneNumber(input) {
+  input.addEventListener("input", function () {
+    let value = input.value.replace(/\D/g, "");
+    let formattedValue = "";
+    let index = 0;
+    let maxLength;
+
+    if (value[0] === "7") {
+      formattedValue += "+7";
+      maxLength = 22;
+      index = 1;
+    } else if (value[0] === "8") {
+      formattedValue += "8";
+      maxLength = 21;
+      index = 1;
+    } else {
+      input.value = formattedValue;
+      return;
+    }
+
+    while (index < value.length && formattedValue.length < maxLength) {
+      if (index === 1) formattedValue += " (";
+      else if (index === 4) formattedValue += ") ";
+      else if (index === 7 || index === 9) formattedValue += " - ";
+      formattedValue += value[index];
+      index++;
+    }
+
+    input.value = formattedValue;
+  });
+}
+
+document.querySelectorAll("[data-tel-input]").forEach(formatPhoneNumber);
+
 restrictedInput();
 hideLayout();
-wrapLastTwoItems();
 
 window.addEventListener("resize", hideLayout);
-window.addEventListener("resize", wrapLastTwoItems);
